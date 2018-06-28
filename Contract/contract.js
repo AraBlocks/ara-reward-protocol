148f9afd926469203479b79a0fc836580c0be4a5
hdao1121 <huy@littlstar.com>
2018-06-28 18:41:41 -0400

    refactor + add boilerplate func

--- Contract/contract.js
+++ Contract/contract.js
@@ -2,23 +2,29 @@ const Web3 = require("web3");
 const ABI = require("./ABI");
 
 class FarmingContract {
-  constructor(contractAddress, moderatorAddress) {
-    this.web3 = new Web3(
-      new Web3.providers.HttpProvider("http://192.168.128.232:7545")
-    );
-    this.contract = new this.web3.eth.Contract(ABI, contractAddress);
-
-    // if (typeof web3 !== "undefined") {
-    //   this.web3 = new Web3(Web3.currentProvider);
-    // } else {
-    //   // set the provider you want from Web3.providers
-    //   this.web3 = new Web3(
-    //     new Web3.providers.HttpProvider("http://192.168.128.232:7545")
-    //   );
-    // }
-
-    this.web3.eth.defaultAccount = moderatorAddress;
+  constructor(contractAddress, personalAddress) {
+    if (typeof this.web3 !== "undefined") {
+      this.web3 = new Web3(this.web3.currentProvider);
+    } else {
+      // set the provider you want from Web3.providers
+      this.web3 = new Web3(
+        new Web3.providers.HttpProvider("http://192.168.128.232:7545")
+      );
+    }
+    this.web3.eth.defaultAccount = personalAddress;
   }
+
+  getBalance() {}
+
+  getRate(jobId) {}
+
+  createJob(jobId, budget) {}
+
+  acceptJob(jobId) {}
+
+  startJob(jobId) {}
+
+  abortJob(jobId) {}
 }
 
 module.exports = FarmingContract;
