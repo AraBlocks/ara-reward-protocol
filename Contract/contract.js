d7a61bf651996101688ccf55a6cab15bd127af5f
hdao1121 <huy@littlstar.com>
2018-06-29 18:03:53 -0400

    add signing method

--- Contract/contract.js
+++ Contract/contract.js
@@ -1,19 +1,44 @@
 const Web3 = require("web3");
-const ABI = require("./ABI");
+const abi = require("./ABI");
+const EthereumTx = require("ethereumjs-tx");
 
 class FarmingContract {
-  constructor(contractAddress, personalAddress) {
+  constructor(contractAddress, personalAddress, personalKey) {
     if (typeof this.web3 !== "undefined") {
       this.web3 = new Web3(this.web3.currentProvider);
     } else {
-      // set the provider you want from Web3.providers
       this.web3 = new Web3(
         new Web3.providers.HttpProvider("http://192.168.128.232:7545")
       );
     }
-    this.contract = new this.web3.eth.Contract(ABI, contractAddress);
+    this.contract = new this.web3.eth.Contract(abi, contractAddress);
     this.web3.eth.defaultAccount = personalAddress;
+
     this.personalAddress = personalAddress;
+    this.contractAddress = contractAddress;
+    this.personalKey = personalKey;
+  }
+
+  sign(abiData, txValue) {
+    const eth = this.web3.eth;
+    this.web3.eth.getTransactionCount(this.personalAddress, function(
+      err,
+      nonce
+    ) {
+      var tx = new EthereumTx({
+        nonce: nonce,
+        gasLimit: 3000000,
+        to: this.contractAddress,
+        value: txValue,
+        data: abiData
+      });
+      tx.sign(Buffer.from(this.personalKey, "hex"));
+
+      var raw = "0x" + tx.serialize().toString("hex");
+      eth.sendSignedTransaction(raw, function(err, transactionHash) {
+        console.log(err, transactionHash);
+      });
+    });
   }
 
   getBalance() {
@@ -41,14 +66,8 @@ class FarmingContract {
   }
 
   createJob(jobId, budget) {
-    this.contract.methods
-      .createJob(jobId)
-      .send(
-        { from: this.personalAddress, value: budget, gas: 3000000 },
-        function(error, result) {
-          console.log("error: ", error, "\nresult: ", result);
-        }
-      );
+    const encodedMethod = this.contract.methods.createJob(jobId).encodeABI();
+    this.sign(encodedMethod, budget);
   }
 
   acceptJob(jobId) {
--- Contract/contract_factory.js
+++ Contract/contract_factory.js
@@ -2,9 +2,8 @@ let Contract = require("./contract");
 
 const contract = new Contract(
   "0xfd1966eb78390209c49767a05c6c799ba2e37fad",
-  "0xa151a089fc8f9f04cc5cea3062c7f0bd10e9e703"
+  "0xa151a089fc8f9f04cc5cea3062c7f0bd10e9e703",
+  "0185fd42264c197154af8f54bf74f8aeea2f777f83f23daa5313772aa7628aff"
 );
 
-contract.createJob(100, 100);
-contract.getJob(100);
-contract.getBalance();
+contract.createJob(123, 100);
