02f363ddfc66af5260c853aed9752c9e7107c868
Brandon Plaster <brandon@littlstar.com>
2018-08-03 20:49:36 -0400

    feat(farmer-stream): functional farmer stream

--- examples/remote-peers-streaming/farmer-stream.js
+++ examples/remote-peers-streaming/farmer-stream.js
@@ -5,20 +5,26 @@ const { messages } = require('ara-farming-protocol')
 const ip = require('ip')
 const through = require('through')
 const { create } = require('ara-filesystem')
-const fp = require("find-free-port")
+const fp = require('find-free-port')
 
+const dids = [
+  'ab5867eeaeacebda573ae252331f4b1b298fd9a8ca883f2b28bad5764f10f99c',
+  '5a0ca463a488b4d3d85ea243087043e1b87b35eae8e15c86c99c4b4d9c14179b'
+]
 
-const did = 'did:ara:1debc451b5bfba29f46bcbbeb9d4957bed0140b6ba56f8d3826b656992f4cb2a' 
-broadcast(did)
+for (let i = 0; i < dids.length; i++){
+  broadcast(dids[i])
+}
 
+async function broadcast(did) {
+  console.log("Broadcasting: ", did)
 
-async function broadcast (did) {
   // Create a swarm for uploading the content
-  const { afs } = await create({did})
+  const { afs } = await create({ did })
 
   // Join the discovery swarm for the requested content
   const opts = {
-      stream,
+    stream,
   }
   const swarm = createSwarm(opts)
   swarm.on('connection', handleConnection)
@@ -27,30 +33,30 @@ async function broadcast (did) {
   function stream(peer) {
     const us = idify(ip.address(), this.address().port)
     const them = idify(peer.host, peer.port)
-  
+
     if (us === them) {
       return through()
     }
-    
+
     const { stream } = new FarmerStream(afs, peer, { wait: 100, timeout: 10000 })
     stream.on('error', onerror)
     stream.on(MSG.SOW.str, onsow)
     stream.on(MSG.QUOTE.str, onquote)
     stream.on(MSG.AGREEMENT.str, onagreement)
     stream.on('end', onend)
-    
+
     function onsow(sow, peer) {
       console.log('sow:', sow.getId(), peer.host, peer.port)
     }
-    
+
     function onquote(quote, peer) {
       console.log('quote:', quote.getPerUnitCost(), peer.host, peer.port)
     }
-    
+
     function onagreement(agreement, peer) {
       console.log('agreement:', agreement.getId(), peer.host, peer.port)
     }
-    
+
     function onend() {
       console.log('end')
     }
@@ -63,24 +69,23 @@ async function broadcast (did) {
     return stream
   }
 
-  function handleConnection(connection, info){
-      console.log(`SWARM: New peer: ${info.host} on port: ${info.port}`)
+  function handleConnection(connection, info) {
+    console.log(`SWARM: New peer: ${info.host} on port: ${info.port}`)
   }
 }
 
-function idify(host, port){
+function idify(host, port) {
   return `${host}:${port}`.replace('::ffff:', '')
 }
 
-
 class FarmerStream extends StreamProtocol {
-  constructor(afs, peer, opts){
+  constructor(afs, peer, opts) {
     super(peer, opts)
     this.afs = afs
     this.swarm = null
   }
 
-  onsow(sow, done){
+  onsow(sow, done) {
     super.onsow(sow, done)
     const quote = new messages.Quote()
     quote.setId(5)
@@ -89,19 +94,19 @@ class FarmerStream extends StreamProtocol {
     this.sendquote(quote)
   }
 
-  async onagreement(agreement, done){
+  async onagreement(agreement, done) {
     super.onagreement(agreement, done)
-    fp(Math.floor(30000 * Math.random()), ip.address()).then(([port]) => {
-      console.log("Listening on port ", port)
+    fp(Math.floor(30000 * Math.random()), ip.address()).then(([ port ]) => {
+      console.log('Listening on port ', port)
       agreement.setId(port) // HACK
       this.sendagreement(agreement, false)
       this.startwork(port)
     })
   }
 
-  async startwork(port){
+  async startwork(port) {
     const opts = {
-        stream: stream.bind(this)
+      stream: stream.bind(this)
     }
     this.swarm = createSwarm(opts)
     this.swarm.on('connection', handleConnection)
@@ -109,19 +114,20 @@ class FarmerStream extends StreamProtocol {
 
     function stream(peer) {
       const stream = this.afs.replicate({
-           upload: true,
-           download: false
+        upload: true,
+        download: false
       })
-      stream.once('end', onend)
-  
-     function onend(){
-       console.log(`Uploaded!`)
-     }
-  
+      stream.once('end', onend.bind(this))
+
+      function onend() {
+        console.log('Uploaded!')
+        this.swarm.destroy()
+      }
+
       return stream
     }
 
-    function handleConnection(connection, info){
+    function handleConnection(connection, info) {
       console.log(`Peer connected: ${info.host} on port: ${info.port}`)
     }
   }
