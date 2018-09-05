10be5f9bfb5b12b47ee7af85a2b1b325c562e667
hdao1121 <huy.dao13@gmail.com>
2018-09-05 14:08:10 -0400

    refactor(): update handshake (#17)

--- examples/utils/handshake-utils.js
+++ examples/utils/handshake-utils.js
@@ -1,5 +1,5 @@
 const { passphrase, networkSecret, networkKeyName } = require('../constants.js')
-const { unpack, keyRing } = require('ara-network/keys')
+const { unpack, keyRing, derive } = require('ara-network/keys')
 const { info, warn } = require('ara-console')
 const { Handshake } = require('ara-network/handshake')
 const { readFile } = require('fs')
@@ -7,6 +7,7 @@ const { resolve } = require('path')
 const { DID } = require('did-uri')
 const crypto = require('ara-crypto')
 const pify = require('pify')
+const ss = require('ara-secret-storage')
 const rc = require('ara-runtime-configuration')()
 
 function configFarmerHandshake(conf) {
@@ -36,11 +37,12 @@ function configRequesterHandshake(conf) {
 
 function getHandshake(conf) {
   const {
-    publicKey, secretKey, secret, unpacked
+    secretKey, secret, unpacked
   } = conf
+  const kp = derive({ secretKey, name: networkKeyName })
   const handshake = new Handshake({
-    publicKey,
-    secretKey,
+    publicKey: kp.publicKey,
+    secretKey: kp.secretKey,
     secret,
     remote: { publicKey: unpacked.publicKey },
     domain: { publicKey: unpacked.domain.publicKey }
@@ -72,7 +74,7 @@ async function unpackKeys(identity, keypath) {
   const path = resolve(rc.network.identity.root, hash, 'keystore/ara')
   const secret = Buffer.from(networkSecret)
   const keystore = JSON.parse(await pify(readFile)(path, 'utf8'))
-  const secretKey = crypto.decrypt(keystore, { key: password.slice(0, 16) })
+  const secretKey = ss.decrypt(keystore, { key: password.slice(0, 16) })
   const keyring = keypath.indexOf('pub') < 0 ? keyRing(keypath, { secret: secretKey }) : keyRing(keypath, { secret })
   if (await keyring.has(networkKeyName)) {
     const buffer = await keyring.get(networkKeyName)
