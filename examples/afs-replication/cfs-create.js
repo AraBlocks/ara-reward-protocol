061cf8315effd386867067fe6e6dfb05b054eeba
hdao1121 <huy@littlstar.com>
2018-10-02 12:47:35 -0400

    wip(): export id and key of farmer's cfs folder

--- examples/afs-replication/cfs-create.js
+++ examples/afs-replication/cfs-create.js
@@ -2,30 +2,25 @@ const { createCFS } = require('cfsnet/create')
 const debug = require('debug')('afp:create')
 const mirror = require('mirror-folder')
 const { join, basename, resolve } = require('path')
+const fs = require('fs')
+
+const path = ('./robot.jpg')
+const afsPath = ('./.ara/afs/farmerAFS')
+const afsId = "farming-docker"
 
 async function create() {
   const afs = await createCFS({
-  id: "test",
-  path: '/Users/huydao/.ara/afs/testAFS'
+  id: afsId,
+  path: afsPath
   })
 
-  await mirrorPath('robot.jpg', afs)
-  // console.log(afs);
-  console.log(afs.key.toString('hex'));
-  // f615a9bcba0d8953cd2fc56add30f0ba85fed751278cfd10330f6ca290f0e02a
-
-  // console.log(discoveryKey.toString('hex'))
-  // const afs2 = await createCFS({
-  // id: "test",
-  // key: afs.key,
-  //
-  // path: '/Users/huydao/.ara/afs/testAFS67'
-  // })
-  // console.log(afs2);
-  // Buffer.from(afs.key, )
-  // console.log(afs.discoveryKey, afs.key);
-  // console.log(afs2.discoveryKey, afs2.key);
-
+  await mirrorPath(path, afs)
+  await fs.writeFile(`./constant.json`, `{id: ${afsId}, key: ${afs.key.toString('hex')}}`, function(err) {
+      if(err) {
+          return console.log(err);
+      }
+      console.log("constant.json was saved in the same folder");
+  })
 }
 
 async function mirrorPath(path, afs) {
@@ -55,3 +50,21 @@ async function mirrorPath(path, afs) {
 }
 
 create()
+
+
+
+// console.log(afs);
+// console.log(afs.key.toString('hex'));
+// f615a9bcba0d8953cd2fc56add30f0ba85fed751278cfd10330f6ca290f0e02a
+
+// console.log(discoveryKey.toString('hex'))
+// const afs2 = await createCFS({
+// id: "test",
+// key: afs.key,
+//
+// path: '/Users/huydao/.ara/afs/testAFS67'
+// })
+// console.log(afs2);
+// Buffer.from(afs.key, )
+// console.log(afs.discoveryKey, afs.key);
+// console.log(afs2.discoveryKey, afs2.key);
