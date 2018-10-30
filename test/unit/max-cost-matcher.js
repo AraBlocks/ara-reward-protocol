530016869b85d40c1f62b98e3fb992afc49ba03c
Brandon Plaster <brandon@littlstar.com>
2018-10-30 12:20:08 -0400

    test(all): renamed, added max-cost-matcher tests

---
+++ test/unit/max-cost-matcher.js
@@ -0,0 +1,74 @@
+const { MaxCostMatcher } = require('../../src/matchers/max-cost-matcher')
+const {
+  messages: {
+    Quote
+  }
+} = require('farming-protocol-buffers')
+const test = require('ava')
+
+test('matcher.addQuote', async (t) => {
+  const matcher = new MaxCostMatcher(5, 5)
+
+  const goodQuoteId = Buffer.from('abcd', 'hex')
+  const goodQuote = new Quote()
+  goodQuote.setNonce(goodQuoteId)
+  goodQuote.setPerUnitCost(5)
+
+  matcher.addQuote(goodQuote, (hire) => {
+    t.true(hire)
+  })
+
+  const badQuoteId = Buffer.from('1234', 'hex')
+  const badQuote = new Quote()
+  badQuote.setNonce(badQuoteId)
+  badQuote.setPerUnitCost(10)
+
+  matcher.addQuote(badQuote, (hire) => {
+    t.false(hire)
+  })
+})
+
+test('matcher.removeQuote', async (t) => {
+  const matcher = new MaxCostMatcher(5, 0)
+
+  const nonceString = 'abcd'
+  const quoteId = Buffer.from(nonceString, 'hex')
+  const quote = new Quote()
+  quote.setNonce(quoteId)
+  quote.setPerUnitCost(5)
+  matcher.addQuote(quote, () => {})
+
+  matcher.removeQuote(quote)
+  t.true(-1 === matcher.reserveWorkers.indexOf(nonceString))
+  t.false(matcher.hiredQuoteCallbacks.has(nonceString))
+  t.true(0 === matcher.hiredQuoteCallbacks.size)
+
+  matcher.removeQuote(quote)
+})
+
+test('matcher.hireFromReserve', async (t) => {
+  const matcher = new MaxCostMatcher(5, 1)
+  matcher.hireFromReserve()
+
+  const nonceString1 = 'abcd'
+  const quoteId1 = Buffer.from(nonceString1, 'hex')
+  const quote1 = new Quote()
+  quote1.setNonce(quoteId1)
+  quote1.setPerUnitCost(5)
+
+  const nonceString2 = '1234'
+  const quoteId2 = Buffer.from(nonceString2, 'hex')
+  const quote2 = new Quote()
+  quote2.setNonce(quoteId2)
+  quote2.setPerUnitCost(5)
+
+  matcher.addQuote(quote1, (hire) => {
+    t.true(hire)
+  })
+  matcher.addQuote(quote2, (hire) => {
+    t.true(hire)
+    t.false(matcher.hiredQuoteCallbacks.has(nonceString1))
+    t.true(matcher.hiredQuoteCallbacks.has(nonceString2))
+  })
+  matcher.removeQuote(quote1)
+})
