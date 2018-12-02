a6ed93111a7372bea2ef7892390c0d766f2c387e
Brandon Plaster <brandon@littlstar.com>
2018-12-02 18:32:38 -0500

    refactor(duplex-connection): compatability with hypercore-protocol streams

--- src/duplex/duplex-connection.js
+++ src/duplex/duplex-connection.js
@@ -4,23 +4,52 @@ const { PeerConnection } = require('../peer-connection')
 const varint = require('varint')
 const debug = require('debug')('afp:duplex')
 
+/*
+* Messages are consistent with hypercore-protocol:
+*   length | numeric-feed-id << 4 | numeric-type | user-type | payload
+*   where numeric-type == 15 (user defined)
+*   https://github.com/mafintosh/hypercore-protocol/blob/master/schema.proto
+*/
+
+const HYPERCORE_USER_TYPE = 15
+
+// Note: communicates over the 0th feed
+const FEED_ID = 0
+
 // Helper object for determining message types
 const MSG = {
-  SOW: { head: 1, str: 'sow' },
-  QUOTE: { head: 2, str: 'quote' },
-  AGREEMENT: { head: 3, str: 'agreement' },
-  REWARD: { head: 4, str: 'reward' },
-  RECEIPT: { head: 5, str: 'receipt' },
+  SOW: { type: 1, str: 'sow' },
+  QUOTE: { type: 2, str: 'quote' },
+  AGREEMENT: { type: 3, str: 'agreement' },
+  REWARD: { type: 4, str: 'reward' },
+  RECEIPT: { type: 5, str: 'receipt' },
 
-  encode(head, data) {
-    head = Buffer.from(varint.encode(head))
-    return Buffer.concat([ head, data ])
+  encode(type, data) {
+    // eslint-disable-next-line no-bitwise
+    const id = Buffer.from(varint.encode(FEED_ID << 4))
+    type = Buffer.from(varint.encode(type))
+    const hyperType = Buffer.from(varint.encode(HYPERCORE_USER_TYPE))
+    const length = Buffer.from(varint.encode(id.length + hyperType.length + type.length + data.length))
+    return Buffer.concat([ length, id, hyperType, type, data ])
   },
 
   decode(chunk) {
-    const head = varint.decode(chunk, 0)
-    const data = chunk.slice(varint.decode.bytes)
-    return { head, data }
+    const length = varint.decode(chunk)
+
+    let data = chunk.slice(varint.decode.bytes)
+    const id = varint.decode(data)
+
+    data = data.slice(varint.decode.bytes)
+    const hyperType = varint.decode(data)
+
+    data = data.slice(varint.decode.bytes)
+    const type = varint.decode(data)
+
+    data = data.slice(varint.decode.bytes)
+
+    return {
+      length, id, hyperType, type, data
+    }
   }
 }
 
@@ -51,31 +80,31 @@ class DuplexConnection extends PeerConnection {
   async sendSow(sow) {
     debug(`Sending Sow: ${nonceString(sow)} to ${this.peerId}`)
     this.timeout = setTimeout(this.onTimeout.bind(this), this.opts.timeout)
-    this.stream.write(MSG.encode(MSG.SOW.head, sow.serializeBinary()))
+    this.stream.write(MSG.encode(MSG.SOW.type, sow.serializeBinary()))
   }
 
   async sendQuote(quote) {
     debug(`Sending Quote: ${nonceString(quote)} to ${this.peerId}`)
     this.timeout = setTimeout(this.onTimeout.bind(this), this.opts.timeout)
-    this.stream.write(MSG.encode(MSG.QUOTE.head, quote.serializeBinary()))
+    this.stream.write(MSG.encode(MSG.QUOTE.type, quote.serializeBinary()))
   }
 
   async sendAgreement(agreement) {
     debug(`Sending Agreement: ${nonceString(agreement)} to ${this.peerId}`)
     this.timeout = setTimeout(this.onTimeout.bind(this), this.opts.timeout)
-    this.stream.write(MSG.encode(MSG.AGREEMENT.head, agreement.serializeBinary()))
+    this.stream.write(MSG.encode(MSG.AGREEMENT.type, agreement.serializeBinary()))
   }
 
   async sendReward(reward) {
     debug(`Sending Reward: ${nonceString(reward)} to ${this.peerId}`)
     this.timeout = setTimeout(this.onTimeout.bind(this), this.opts.timeout)
-    this.stream.write(MSG.encode(MSG.REWARD.head, reward.serializeBinary()))
+    this.stream.write(MSG.encode(MSG.REWARD.type, reward.serializeBinary()))
   }
 
   async sendReceipt(receipt) {
     debug(`Sending Receipt: ${nonceString(receipt)} to ${this.peerId}`)
     this.timeout = setTimeout(this.onTimeout.bind(this), this.opts.timeout)
-    this.stream.write(MSG.encode(MSG.RECEIPT.head, receipt.serializeBinary()))
+    this.stream.write(MSG.encode(MSG.RECEIPT.type, receipt.serializeBinary()))
   }
 
   async close() {
@@ -135,14 +164,18 @@ class DuplexConnection extends PeerConnection {
 
   onData(chunk) {
     try {
-      const { head, data } = MSG.decode(chunk)
-      switch (head) {
-      case MSG.SOW.head: this.onSow(messages.SOW.deserializeBinary(data)); break
-      case MSG.QUOTE.head: this.onQuote(messages.Quote.deserializeBinary(data)); break
-      case MSG.AGREEMENT.head: this.onAgreement(messages.Agreement.deserializeBinary(data)); break
-      case MSG.REWARD.head: this.onReward(messages.Reward.deserializeBinary(data)); break
-      case MSG.RECEIPT.head: this.onReceipt(messages.Receipt.deserializeBinary(data)); break
-      default: throw new TypeError(`Unknown message type: ${head}`)
+      const {
+        id, hyperType, type, data
+      } = MSG.decode(chunk)
+      if (HYPERCORE_USER_TYPE != hyperType || FEED_ID != id) return false
+
+      switch (type) {
+      case MSG.SOW.type: this.onSow(messages.SOW.deserializeBinary(data)); break
+      case MSG.QUOTE.type: this.onQuote(messages.Quote.deserializeBinary(data)); break
+      case MSG.AGREEMENT.type: this.onAgreement(messages.Agreement.deserializeBinary(data)); break
+      case MSG.REWARD.type: this.onReward(messages.Reward.deserializeBinary(data)); break
+      case MSG.RECEIPT.type: this.onReceipt(messages.Receipt.deserializeBinary(data)); break
+      default: throw new TypeError(`Unknown message type: ${type}`)
       }
       return true
     } catch (e) {
