34540c99024e9c4500ddcb779ee08c8c63503625
Brandon Plaster <brandon@littlstar.com>
2019-01-11 11:55:33 -0500

    feat(package): bumped reward protocol to use string as price

--- test/unit/max-cost-matcher.js
+++ test/unit/max-cost-matcher.js
@@ -6,32 +6,35 @@ const {
 } = require('reward-protocol-buffers')
 const test = require('ava')
 
+const LOW_COST = '5.000000005'
+const HIGH_COST = '5.000000006'
+
 test('matcher.addQuote', async (t) => {
-  const matcher = new MaxCostMatcher(5, 5)
+  const matcher = new MaxCostMatcher(LOW_COST, 5)
 
   const goodQuoteId = Buffer.from('abcd', 'hex')
   const goodQuote = new Quote()
   goodQuote.setNonce(goodQuoteId)
-  goodQuote.setPerUnitCost(5)
+  goodQuote.setPerUnitCost(LOW_COST)
 
   t.true(await matcher.addQuote(goodQuote, () => {}))
 
   const badQuoteId = Buffer.from('1234', 'hex')
   const badQuote = new Quote()
   badQuote.setNonce(badQuoteId)
-  badQuote.setPerUnitCost(10)
+  badQuote.setPerUnitCost(HIGH_COST)
 
   t.false(await matcher.addQuote(badQuote, () => {}))
 })
 
 test('matcher.removeQuote', async (t) => {
-  const matcher = new MaxCostMatcher(5, 0)
+  const matcher = new MaxCostMatcher(LOW_COST, 0)
 
   const nonceString = 'abcd'
   const quoteId = Buffer.from(nonceString, 'hex')
   const quote = new Quote()
   quote.setNonce(quoteId)
-  quote.setPerUnitCost(5)
+  quote.setPerUnitCost(LOW_COST)
   matcher.addQuote(quote, () => {})
 
   matcher.removeQuote(quote)
@@ -43,20 +46,20 @@ test('matcher.removeQuote', async (t) => {
 })
 
 test('matcher.hireFromReserve', async (t) => {
-  const matcher = new MaxCostMatcher(5, 1)
+  const matcher = new MaxCostMatcher(LOW_COST, 1)
   matcher.hireFromReserve()
 
   const nonceString1 = 'abcd'
   const quoteId1 = Buffer.from(nonceString1, 'hex')
   const quote1 = new Quote()
   quote1.setNonce(quoteId1)
-  quote1.setPerUnitCost(5)
+  quote1.setPerUnitCost(LOW_COST)
 
   const nonceString2 = '1234'
   const quoteId2 = Buffer.from(nonceString2, 'hex')
   const quote2 = new Quote()
   quote2.setNonce(quoteId2)
-  quote2.setPerUnitCost(5)
+  quote2.setPerUnitCost(LOW_COST)
 
   t.true(await matcher.addQuote(quote1, () => {}))
   t.true(await matcher.addQuote(quote2, () => {}))
